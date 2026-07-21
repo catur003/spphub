231a05bbc7960805587e5ee37a91b8cad4413f1c
Catur <caturpamungkas103@gmail.com>
2026-07-21 23:40:25 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
+    const cspHeader = `
+      default-src 'self' https: http: data: blob: 'unsafe-inline';
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https: http:;
+      style-src 'self' 'unsafe-inline' https: http:;
+      img-src 'self' data: blob: https: http:;
+      font-src 'self' data: https: http:;
+      frame-src 'self' https: http:;
+      connect-src 'self' https: http:;
+    `.replace(/\s{2,}/g, ' ').trim();
+
     return [
       {
         source: '/(.*)',
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' https: http: data: blob:;"
+            value: cspHeader
           }
         ],
       },
