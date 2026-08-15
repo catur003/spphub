50b48c2e9691712018803e0e27533dc12c8133d2
Catur <56010480+catur003@users.noreply.github.com>
2026-08-15 21:38:28 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="id">
       <body>{children}
-      <script defer src="https://zenin.my.id/track.js?v=2" data-site="cmsugskcv000lem1hdc7s873p"></script>
+      <script defer src="https://zenin.my.id/track.js?v=3" data-site="cmsugskcv000lem1hdc7s873p"></script>
       </body>
     </html>
   );
